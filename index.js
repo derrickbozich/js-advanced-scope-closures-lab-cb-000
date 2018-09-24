3bb478e8ba831568af7c43f5d2c874e2648184a5
derrick b <dbozich1@gmail.com>
2018-09-24 06:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function produceDrivingRange(blockRange){
 
     let difference = blockRange - rideDistance;
     let result = difference > 0 ? `within range by ${difference}` : `${Math.abs(difference)} blocks out of range`
-
+    return result
   }
 
 }
