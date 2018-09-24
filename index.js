25abf7c2eab62f14b8be22caef063d6f734dee1a
derrick b <dbozich1@gmail.com>
2018-09-24 06:58:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function produceDrivingRange(blockRange){
   return (startingBlock, endingBlock) => {
-    debugger
+
     let start = parseInt(startingBlock.slice(0, -2));
     let end = parseInt(endingBlock.slice(0, -2));
+    let rideDistance = Math.abs(start - end);
 
     let difference = blockRange - rideDistance;
     let result = difference > 0 ? `within range by ${difference}` : `${Math.abs(difference)} blocks out of range`
