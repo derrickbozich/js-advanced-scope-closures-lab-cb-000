f5d753885d70077366100096abddf4ec7d4035cb
derrick b <dbozich1@gmail.com>
2018-09-24 06:59:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ function produceDrivingRange(blockRange){
 
     let difference = blockRange - rideDistance;
     let result = difference > 0 ? `within range by ${difference}` : `${Math.abs(difference)} blocks out of range`
-    return result
+    return result;
   }
+}
 
+function produceTipCalculator(){
+  return () => {
+    
+  }
 }
